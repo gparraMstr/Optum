352c8a8a15c98ab73b1115df8a84aba30b9cf97d
gparraMstr <gparra@microstrategy.com>
2016-11-22 15:00:54 -0500

    Updating files

--- javascript/mojo/js/source/MstrVisD3MultiChart.js
+++ javascript/mojo/js/source/MstrVisD3MultiChart.js
@@ -4,59 +4,63 @@
  */
 (function () {
     // We need to define this code as plugin in mstrmojo object
-    if (!mstrmojo.plugins.MstrVisD3MultiChart) {
-        mstrmojo.plugins.MstrVisD3MultiChart = {};
+    if (!mstrmojo.plugins.Optum) {
+        mstrmojo.plugins.Optum = {};
     }
     // Visualization requires library to render, and in this
     mstrmojo.requiresCls("mstrmojo.CustomVisBase");
     // Declaration of the visualization object
-    mstrmojo.plugins.MstrVisD3MultiChart.MstrVisD3MultiChart = mstrmojo.declare(
+    mstrmojo.plugins.Optum.MstrVisD3MultiChart = mstrmojo.declare(
         //We need to declare that our code extends CustomVisBase
         mstrmojo.CustomVisBase,
         null,
         {
             //here scriptClass is defined as mstrmojo.plugins.{plugin name}.{js file name}
-            scriptClass: 'mstrmojo.plugins.MstrVisD3MultiChart.MstrVisD3MultiChart',
+            scriptClass: 'mstrmojo.plugins.Optum.MstrVisD3MultiChart',
             model: null,
             cssClass: "MstrVisD3MultiChart",
-            errorDetails: "This visualization requires at least 3 metrics.",
+            errorDetails: "This visualization requires at least 1 metrics.",
             useRichTooltip: true,
             reuseDOMNode: true,
             externalLibraries: [
                 {
-                    url: "http://d3js.org/d3.v3.min.js",
-                    url: "../plugins/Optum/javascript/bullet.js",
-                    url: "https://rawgit.com/gka/d3-jetpack/master/d3-jetpack.js"                       
+                    url: "../plugins/Optum/javascript/d3.v3.min.js"              
+                },
+                {
+                    url: "../plugins/Optum/javascript/bullet.js"              
                 }
             ],
             /**
             * Rendering Multichart using D3 JS framework for Optum project 
             */
-            plot: function () {
+            plot: function () { 
                 //...YOUR JS CODE...
                 this.domNode.innerText = "Empty text";
 
                 // Define this code as a plugin in the mstrmojo object
-                if (!mstrmojo.plugins.MstrVisD3MultiChart) {
-                    mstrmojo.plugins.MstrVisD3MultiChart = {};
+                if (!mstrmojo.plugins.Optum) {
+                    mstrmojo.plugins.Optum = {};
                 }
 
                 if (this.domNode.childNodes.length === 1) {
-                     this.domNode.removeChild(this.domNode.childNodes[0]);
+                    this.domNode.removeChild(this.domNode.childNodes[0]);
                 }
                 
                 var margin = {top: 10, right: 30, bottom: 50, left: 80},
                      width = parseInt(this.width,10) - margin.left - margin.right,
                      height = parseInt(this.height,10) - margin.top - margin.bottom;
 
+
                 var chart = d3.select(this.domNode).append("svg").attr("width", width + margin.left + margin.right)
                      .attr("height", height + margin.top + margin.bottom)
                      .append("g")
                      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
                 
+                debugger;
                 var x = d3.scale.ordinal()
                      .rangeRoundBands([0, width], 0.1);
-                    
+                 
+
                 var data = this.dataInterface.getRawData(mstrmojo.models.template.DataInterface.ENUM_RAW_DATA_FORMAT.TREE).children;
                 var metricName = this.dataInterface.getColHeaders(0).getHeader(0).getName();
                 
