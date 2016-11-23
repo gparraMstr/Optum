e3ee76d929f78733ea0b090944a86505d1f05939
gparraMstr <gparra@microstrategy.com>
2016-11-23 14:25:07 -0500

    Improving UI
    Added changes to support Grid and read data from MSTR Dataset…

--- javascript/mojo/js/source/MstrVisD3MultiChart.js
+++ javascript/mojo/js/source/MstrVisD3MultiChart.js
@@ -51,19 +51,89 @@
                      height = parseInt(this.height,10) - margin.top - margin.bottom;
 
 
-                var chart = d3.select(this.domNode).append("svg").attr("width", width + margin.left + margin.right)
+               /* var chart = d3.select(this.domNode).append("svg").attr("width", width + margin.left + margin.right)
                      .attr("height", height + margin.top + margin.bottom)
                      .append("g")
-                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); */
+
+                // create table
                 
-                debugger;
                 var x = d3.scale.ordinal()
                      .rangeRoundBands([0, width], 0.1);
-                 
 
-                var data = this.dataInterface.getRawData(mstrmojo.models.template.DataInterface.ENUM_RAW_DATA_FORMAT.TREE).children;
+                var data = this.dataInterface.getRawData(mstrmojo.models.template.DataInterface.ENUM_RAW_DATA_FORMAT.ROWS);
+                var columns = this.dataInterface. getColumnHeaderData();
+
                 var metricName = this.dataInterface.getColHeaders(0).getHeader(0).getName();
-                
+
+
+                var multichart = d3.bullet()
+                    .width(width)
+                    .height(35);
+
+                var svg = d3.select(this.domNode).selectAll("svg")
+                    .data([{"title":"Revenue","subtitle":"US$, in thousands","ranges":[150,225,300],"measures":[220,270],"markers":[250]}])
+                    .enter().append("svg")
+                    .attr("class", "bullet")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", 80)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                    .call(multichart);
+
+                var table = d3.select(this.domNode)
+                    .append('table');
+
+                // create table header
+                table.append('thead').append('tr')
+                    .selectAll('th')
+                    .data(columns).enter()
+                    .append('th')
+                    .attr('class', function(d) {
+                        return (d['otp'] == 5 ? 'num':'title');
+                    })
+                    .text(function(d) {
+                        return d['n'];
+                    });
+
+                // create table body
+                table.append('tbody')
+                    .attr("height", height - 80)
+                    .selectAll('tr')
+                    .data(data).enter()
+                    .append('tr')
+                    .selectAll('td')
+                    .data(function(row, i) {
+                        return columns.map(function(c) {
+                            // compute cell values for this specific row
+                            var cell = {};
+
+                            if (typeof row[c['n']] != 'undefined') {
+                                if ((c['otp'] == 12) && (typeof row[c['n']] == "string")) {
+                                    cell['html'] = row[c['n']];
+                                    cell['cl'] = 'title';
+                                } else {
+                                    cell['html'] = row[c['n']]['v'];
+                                    cell['cl'] = 'num';
+                                }
+                            }
+                          /*  d3.keys(c).forEach(function(k) {
+                                cell[k] = typeof c[k] == 'function' ? c[k](row,i) : row[c[k]];
+                            }); */
+
+                            return cell;
+                        });
+                    }).enter()
+                    .append('td')
+                    .html(function(d) { 
+                        return d['html'];
+                    })
+                    .attr('class', function(d) {
+                        return d['cl'];
+                    });
+
+
+
                 x.domain(data.map(function(d) {
                     return d.name;
                 }));
@@ -80,7 +150,7 @@
                 var yAxis = d3.svg.axis()
                     .scale(y)
                     .orient("left");
-                
+                /*
                 chart.append("g")
                     .attr("class", "x axis")
                     .attr("transform", "translate(0," + height + ")")
@@ -108,7 +178,14 @@
                     .attr("height", function(d) {
                         return height - y(d.value);
                     })
-                    .attr("width", x.rangeBand());                
+                    .attr("width", x.rangeBand());       
+                    */         
             }
         });
 })();
+
+function fetch(d) { debugger;
+    var i=0, l = functions.length;
+    while (i++ < l) d = functions[i-1].call(this, d);
+    return d;
+};
